acb564e4ec70c26eb8413d927da29d86ddb01a27
Ethan <ethohampton@gmail.com>
2017-09-22 18:29:36 -0700

    Updated Version to Beta-v4

--- secret/src/main/java/com/ethohampton/secret/Util/Constants.java
+++ secret/src/main/java/com/ethohampton/secret/Util/Constants.java
@@ -12,13 +12,13 @@ public class Constants {
     public static final Long UPDATE_TIME = 30 * 1000L;//30 seconds in milliseconds wait between updates
     public static final int UPDATE_COUNT_LIMIT = 500;//limit to 500 records so we don't overload on memory
 
-    public static final boolean FILTER_WORDS = true;
+    public static final boolean FILTER_WORDS = true;//setting this to false disables the word filter
 
     public static final int MAX_SECRET_LENGTH = 2000;//characters max for a secret
     public static final int MIN_SECRET_LENGTH = 6;//characters min for a secret
 
     public static final int MAX_COOKIE_AGE = 60 * 60 * 24 * 4;//4 days till you have to enter another secret
-    public static final int MAX_SEEN_SECRETS_SIZE = 50;//max seen secrets before cookie gets too big
+    public static final int MAX_SEEN_SECRETS_SIZE = 75;//max seen secrets before cookie gets too big
 
     public static final String BAD_WORDS_FILENAME = "WEB-INF/badwords.cvs";
     public static final String BAD_HASHES_FILENAME = "WEB-INF/banned_hashes.txt";
